d16d98bec77e0cb64ecc9a9e4ccb5bd9ac1dbb58
Violetta Kruglikova <81808301+blindsphynx@users.noreply.github.com>
2023-09-11 17:49:30 +0300

    Update main.py

--- src/server/main.py
+++ src/server/main.py
@@ -1,5 +1,4 @@
-from Server import server, connect_to_postgresql, DBconnection, DBcursor
+from Server import server
 
 if __name__ == '__main__':
-    connect_to_postgresql(DBconnection, DBcursor)
     server.run(debug=True, port=8000)
